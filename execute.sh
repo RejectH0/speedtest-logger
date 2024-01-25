7ffbc667abd3a39fc06aeb87be402c9395d6c38a
RejectH0 <github@lawlynn.com>
2024-01-25 04:49:31 +0000

    execute.sh: Validated and versioned

--- execute.sh
+++ execute.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#
+# Version 1.0 - 20240124-2200
+# This script is used to kick off the log_speedtest.py script.
+#
 cd /usr/local/speedtest-logger
 /usr/local/speedtest-logger/bin/python3 ./log_speedtest.py
